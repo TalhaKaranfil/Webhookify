df466e0028c7daf3e3d0f7d55d37c05aa9ac8081
Talha Karanfil <117168525+TalhaKaranfil@users.noreply.github.com>
2024-05-14 21:10:33 +0200

    create hash, timestamp for serialization

--- backend/src/main/java/com/webhookify/server/model/Webhook.java
+++ backend/src/main/java/com/webhookify/server/model/Webhook.java
@@ -1,10 +1,13 @@
 package com.webhookify.server.model;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 public class Webhook {
 
     private Map<String, String> formValues;
+    private LocalDateTime timestamp;
+    private String hash;
 
     public Map<String, String> getFormValues() {
         return formValues;
@@ -12,5 +15,21 @@ public class Webhook {
 
     public void setFormValues(Map<String, String> formValues) {
         this.formValues = formValues;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
     }
 }
--- backend/src/main/java/com/webhookify/server/service/WebhookService.java
+++ backend/src/main/java/com/webhookify/server/service/WebhookService.java
@@ -3,7 +3,12 @@ package com.webhookify.server.service;
 import com.webhookify.server.model.Webhook;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.List;
 import java.util.Map;
 
@@ -15,10 +20,30 @@ public class WebhookService {
     public void processWebhook(Map<String, String> formValues) {
         Webhook webhook = new Webhook();
         webhook.setFormValues(formValues);
+        webhook.setTimestamp(LocalDateTime.now());
+        webhook.setHash(generateHash(formValues.toString()));
         webhooks.add(webhook);
     }
 
     public List<Webhook> getWebhooks() {
         return webhooks;
+    }
+
+    private String generateHash(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = md.digest(input.getBytes(StandardCharsets.UTF_8));
+            return byteArray2Hex(hashBytes).substring(0, 6);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String byteArray2Hex(byte[] bytes) {
+        Formatter formatter = new Formatter();
+        for (byte b : bytes) {
+            formatter.format("%02x", b);
+        }
+        return formatter.toString();
     }
 }
