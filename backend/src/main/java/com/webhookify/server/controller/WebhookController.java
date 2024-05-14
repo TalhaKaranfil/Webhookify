557d1f4369e3cb4f0c2c817d3ade35cd8f18e587
Talha Karanfil <117168525+TalhaKaranfil@users.noreply.github.com>
2024-05-14 21:32:13 +0200

    add swaggerUI as web interface

--- backend/src/main/java/com/webhookify/server/controller/WebhookController.java
+++ backend/src/main/java/com/webhookify/server/controller/WebhookController.java
@@ -2,14 +2,16 @@ package com.webhookify.server.controller;
 
 import com.webhookify.server.model.Webhook;
 import com.webhookify.server.service.WebhookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-
 @RestController
 @RequestMapping("/api/webhooks")
+@Tag(name = "Webhookify API", description = "API for managing webhooks")
 public class WebhookController {
 
     private final WebhookService webhookService;
@@ -19,12 +21,14 @@ public class WebhookController {
     }
 
     @PostMapping
-    public String receiveWebhook(@RequestParam Map<String, String> formValues) {
+    @Operation(summary = "Send a webhook", description = "Sends a webhook and stores it in runtime memory")
+    public String sendWebhook(@RequestParam Map<String, String> formValues) {
         webhookService.processWebhook(formValues);
         return "Webhook received successfully!";
     }
 
     @GetMapping
+    @Operation(summary = "Get all webhooks", description = "Retrieves all stored webhooks from runtime memory")
     public List<Webhook> getWebhooks() {
         return webhookService.getWebhooks();
     }
