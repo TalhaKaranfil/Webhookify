50df3f9c3558d80f9bcb763858c24d1d213cd787
Talha Karanfil <117168525+TalhaKaranfil@users.noreply.github.com>
2024-05-15 19:39:51 +0200

    implement config logic

--- client/src/main/kotlin/view/ConfigurationScreen.kt
+++ client/src/main/kotlin/view/ConfigurationScreen.kt
@@ -1,16 +1,13 @@
 package view
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import ui.theme.CustomColorPalette
 import viewmodel.ConfigurationViewModel
 
@@ -21,26 +18,43 @@ fun ConfigurationScreen(viewModel: ConfigurationViewModel) {
         verticalArrangement = Arrangement.SpaceEvenly,
         modifier = Modifier.fillMaxSize()
     ) {
-        URLInput()
+        URLInput(viewModel)
+        if (viewModel.message.isNotEmpty()) {
+            Text(
+                text = viewModel.message,
+                color = Color(viewModel.messageColor),
+                style = MaterialTheme.typography.body1
+            )
+        }
     }
 }
 
-@Preview
 @Composable
-fun URLInput() {
-    var text by remember { mutableStateOf("https://") }
+fun URLInput(viewModel: ConfigurationViewModel) {
+    var text by remember { mutableStateOf(viewModel.url) }
 
-    OutlinedTextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text("URL") },
-        singleLine = true,
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = Color(0xFF04d63a),
-            unfocusedBorderColor = CustomColorPalette.onBackground,
-            cursorColor = Color(0xFF04d63a),
-            unfocusedLabelColor = CustomColorPalette.onBackground,
-            focusedLabelColor = Color(0xFF04d63a),
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(30.dp)
+
+    ) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("URL") },
+            singleLine = true,
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color(0xFF04d63a),
+                unfocusedBorderColor = CustomColorPalette.onBackground,
+                cursorColor = Color(0xFF04d63a),
+                unfocusedLabelColor = CustomColorPalette.onBackground,
+                focusedLabelColor = Color(0xFF04d63a),
+            ),
+            modifier = Modifier.weight(1f)
         )
-    )
+        Spacer(modifier = Modifier.width(8.dp))
+        Button(onClick = { viewModel.updateURL(text) }) {
+            Text("Save")
+        }
+    }
 }
--- client/src/main/kotlin/viewmodel/ConfigurationViewModel.kt
+++ client/src/main/kotlin/viewmodel/ConfigurationViewModel.kt
@@ -3,12 +3,68 @@ package viewmodel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import java.io.File
 
 class ConfigurationViewModel {
-    var text by mutableStateOf("Hello, World!")
+
+    var url by mutableStateOf("")
         private set
 
-    fun updateText() {
-        text = "Hello, Desktop!"
+    var message by mutableStateOf("")
+        private set
+
+    var messageColor by mutableStateOf(0xFF000000)
+        private set
+
+    init {
+        loadUrlFromFile()
+    }
+
+    fun updateURL(newURL: String) {
+        url = newURL
+        println("URL updated to $url")
+        saveUrlToFile(url)
+    }
+
+    private fun saveUrlToFile(url: String) {
+        try {
+            // get the users documents directory
+            val documentsDir = File(System.getProperty("user.home"), "Documents/webhookify")
+            if (!documentsDir.exists()) { // if the directory does not exist, create it
+                documentsDir.mkdirs()
+            }
+            // create a file in the documents directory
+            val configFile = File(documentsDir, "webhookify.config")
+            configFile.writeText("url=$url")
+            println("URL saved to ${configFile.absolutePath}")
+            message = "URL saved successfully to ${configFile.absolutePath}"
+            messageColor = 0xFF04d63a // Green
+        } catch (e: Exception) {
+            println("Error saving URL to file: ${e.message}")
+            message = "Failed to save URL"
+            messageColor = 0xFFFF0000 // Red
+        }
+    }
+
+    private fun loadUrlFromFile() {
+        try {
+            // get the users config file
+            val documentsDir = File(System.getProperty("user.home"), "Documents/webhookify")
+            val configFile = File(documentsDir, "webhookify.config")
+            if (configFile.exists()) {
+                // if the file exists, read the URL from it
+                val configLines = configFile.readLines()
+                for (line in configLines) {
+                    if (line.startsWith("url=")) {
+                        // extract the URL from the line and display it
+                        url = line.substringAfter("url=")
+                        println("URL loaded from file: $url")
+                        break
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            println("Error loading URL from file: ${e.message}")
+        }
     }
 }
