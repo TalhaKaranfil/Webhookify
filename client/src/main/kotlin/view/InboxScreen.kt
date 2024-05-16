f6181f9fc10759eb697d7a8eea64c60bee409c0b
Talha Karanfil <117168525+TalhaKaranfil@users.noreply.github.com>
2024-05-16 04:51:21 +0200

    Create progress line to visualize scroll progress

--- client/src/main/kotlin/view/InboxScreen.kt
+++ client/src/main/kotlin/view/InboxScreen.kt
@@ -1,5 +1,6 @@
 package view
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -11,10 +12,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import kotlinx.coroutines.delay
 import ui.theme.CustomColorPalette
 import ui.theme.CustomTypography
@@ -37,12 +41,34 @@ fun InboxScreen(viewModel: InboxViewModel) {
         }
     }
 
+    // Calculate the line length based on scroll progress
+    val lineLength by derivedStateOf {
+        val maxScroll = scrollState.maxValue.toFloat()
+        val currentScroll = scrollState.value.toFloat()
+        if (maxScroll == 0f) 0f else (currentScroll / maxScroll) * 100f
+    }
+
     Box(modifier = Modifier.fillMaxSize()) {
+        Canvas(modifier = Modifier
+            .fillMaxWidth()
+            .height(4.dp)
+            .zIndex(1f) // Ensure the line is on top and cant be overlapped by msg box
+        ) {
+            drawLine(
+                color = Color(0xFF04d63a),
+                start = Offset(0f, 0f),
+                end = Offset(size.width * (lineLength / 100f), 0f),
+                strokeWidth = 4.dp.toPx(),
+                cap = StrokeCap.Round
+            )
+        }
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .verticalScroll(scrollState)
-                .padding(16.dp)
+                .padding(top = 12.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+                .zIndex(0f) // Ensure the messages are below the line
         ) {
             viewModel.messages.asReversed().forEachIndexed { index, message ->
                 val backgroundColor = if (index % 2 == 0) Color(0xFF3b3b3b) else CustomColorPalette.primary
